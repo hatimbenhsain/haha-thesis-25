d70c8205026c6e0efeba9f8fc4ed8c63a7212fa5
HanwenXuMame <128509287+HanwenXuMame@users.noreply.github.com>
2024-10-01 16:38:40 -0400

    added more sex features, rope now works properly

--- SwimmingGame/Assets/Scripts/SexPrototype/SexRopeCursorController.cs
+++ SwimmingGame/Assets/Scripts/SexPrototype/SexRopeCursorController.cs
@@ -5,9 +5,9 @@ using Obi;
 
 public class SexRopeCursorController : MonoBehaviour
 {
-    public GameObject ropeOrigin; 
-    public GameObject swimCharacter; 
-    public float speedMultiplier = 1f; 
+    public GameObject ropeOrigin;
+    public GameObject swimCharacter;
+    public float speedMultiplier = 1f;
     public int pooledParticles = 100; // Maximum number of particles that can be generated
 
     private ObiRopeCursor cursor;
@@ -21,19 +21,21 @@ public class SexRopeCursorController : MonoBehaviour
 
     void Update()
     {
-        // Calculate the distance from the character to the rope origin
-        float distance = Vector3.Distance(swimCharacter.transform.position, ropeOrigin.transform.position);
 
         // Determine the speed of the character (assuming using Rigidbody)
         float characterSpeed = swimCharacter.GetComponent<Rigidbody>().velocity.magnitude;
 
         // Calculate the desired length change
-        float lengthChange = distance * speedMultiplier * characterSpeed * Time.deltaTime;
-        Debug.Log(rope.particleCount);
-        // Check current particle count and change length accordingly
-        if (rope.particleCount < (2*pooledParticles))
+        float lengthChange = speedMultiplier * characterSpeed * Time.deltaTime;
+
+        // Only add rope length when space is pressed
+        if (Input.GetKey(KeyCode.Space))
         {
-            cursor.ChangeLength(lengthChange);
+            // Check if particle count is below the pooled particles limit
+            if (rope.particleCount < (2 * pooledParticles))
+            {
+                cursor.ChangeLength(lengthChange);
+            }
         }
     }
 }
