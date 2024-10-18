00304ffbaf540e399b9774a6cb8fb3b5c45bc1f7
hatimbenhsain <hb1487@nyu.edu>
2024-10-18 01:15:26 -0400

    npc sings

--- SwimmingGame/Assets/Scripts/NPC/NPCSinging.cs
+++ SwimmingGame/Assets/Scripts/NPC/NPCSinging.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using FMOD.Studio;
 using FMODUnity;
+using UnityEditor.EditorTools;
 
 public class NPCSinging : Singing
 {
@@ -25,42 +26,93 @@ public class NPCSinging : Singing
 
     public bool singing=false;
 
+    private SwimmerSinging swimmerSinging;
+    [Tooltip("Max distance to detect swimmer when singing to harmonize.")]
+    public float maxSwimmerDistance=2f;
+
+    public float harmonyValue;
+    [Tooltip("If player is harmonizing for this long (in seconds) the dialogue may start.")]
+    public float harmonyMaxValue=1f;
+
+    public bool canSing=true;
+
 
     void Start()
     {
         SingingStart();
+
+        foreach(var e in events.Values){
+            RuntimeManager.AttachInstanceToGameObject(e,transform,GetComponent<Rigidbody>());
+        }
+
+        swimmerSinging=FindObjectOfType<SwimmerSinging>();
     }
 
     void Update()
     {
-        timer+=Time.deltaTime;
-        timer=timer%(pauseLength+singingLength);
+        if(canSing){
+            timer+=Time.deltaTime;
+            timer=timer%(pauseLength+singingLength);
 
-        bool prevSinging=singing;
+            bool prevSinging=singing;
 
-        if(timer<=singingLength){
-            singing=true;
-            singingVolume=1f;
-        }else{
-            singing=false;
-            singingVolume=0f;
+            if(timer<=singingLength){
+                singing=true;
+                singingVolume=1f;
+            }else{
+                singing=false;
+                singingVolume=0f;
+            }
+
+            if(singing && !prevSinging){
+                StopAllNotes();
+                PlayNote(singingNote);
+                targetOpacity=1f;
+            }else if(!singing){
+                StopAllNotes();
+                targetOpacity=0f;
+            }
+
+            singingTraceSpriteRenderer.sprite=singingTraceSprites[possibleNotes.IndexOf(singingNote)];
+
+            SingingUpdate();
+
+            // Checking harmony and starting dialogue if harmony is achieved
+            // Right now harmony only goes up/down if the npc is currently
+            if(singing && swimmerSinging.singing && Vector3.Distance(transform.position,swimmerSinging.transform.position)<=maxSwimmerDistance){
+                if(isHarmonizing(swimmerSinging)){
+                    harmonyValue+=Time.deltaTime;
+                }
+                if(harmonyValue>=harmonyMaxValue){
+                    DialogueStart();
+                }
+            }else if(singing){
+                harmonyValue-=Time.deltaTime/2f;
+            }
+
+            harmonyValue=Mathf.Clamp(harmonyValue,0f,harmonyMaxValue);
         }
 
-        if(singing && !prevSinging){
-            StopAllNotes();
-            PlayNote(singingNote);
-            targetOpacity=1f;
-        }else if(!singing){
-            StopAllNotes();
-            targetOpacity=0f;
-        }
-
-        singingTraceSpriteRenderer.sprite=singingTraceSprites[possibleNotes.IndexOf(singingNote)];
-
+        
         Color c=singingTraceSpriteRenderer.color;
         float a=Mathf.Lerp(c.a,targetOpacity,imageOpacityLerpSpeed*Time.deltaTime);
         singingTraceSpriteRenderer.color=new Color(c.r,c.g,c.b,a);
+    }
 
-        SingingUpdate();
+    bool isHarmonizing(Singing s){
+        string otherNote=s.singingNote;
+        int index=possibleNotes.IndexOf(singingNote);
+        if(otherNote==possibleNotes[(index+2)%possibleNotes.Count] || otherNote==possibleNotes[(index+possibleNotes.Count-2)%possibleNotes.Count]){
+            return true;
+        }
+        return false;
+    }
+
+    void DialogueStart(){
+        StopAllNotes();
+        canSing=false;
+        harmonyValue=0f;
+        targetOpacity=0f;
+        Debug.Log("Start dialogue!");
     }
 }
