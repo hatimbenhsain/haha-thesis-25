b33687ae2206db4e247d87ad7b3f5f59d7d1fbe3
hatimbenhsain <hb1487@nyu.edu>
2024-12-03 14:11:19 -0500

    making main act 1 tutorial

--- SwimmingGame/Assets/Scripts/UI/Tutorialization/TutorialTrigger.cs
+++ SwimmingGame/Assets/Scripts/UI/Tutorialization/TutorialTrigger.cs
@@ -6,14 +6,14 @@ public class TutorialTrigger : MonoBehaviour
 {
 
     private void OnTriggerEnter(Collider other) {
-        if(other.gameObject.tag=="Player"){
+        if(other.gameObject.tag=="Player" || other.transform.parent.tag=="Player"){
             FindObjectOfType<Tutorial>().EnteredTrigger(GetComponent<Collider>());
             Debug.Log("Entered player");
         }
     }
 
     private void OnTriggerExit(Collider other) {
-        if(other.gameObject.tag=="Player"){
+        if(other.gameObject.tag=="Player" || other.transform.parent.tag=="Player"){
             FindObjectOfType<Tutorial>().ExitedTrigger(GetComponent<Collider>());
             Debug.Log("Exited player");
         }
