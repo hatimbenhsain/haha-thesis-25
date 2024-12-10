73069d6fab4731ff03cde0ad27317b452e5259b7
hatimbenhsain <hb1487@nyu.edu>
2024-12-10 00:31:13 -0500

    convex experiments

---
+++ SwimmingGame/Assets/CoACD/Runtime/CoACDColliderData.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEditor;
+
+public class CoACDColliderData : ScriptableObject
+{
+    public CoACD.Parameters parameters;
+    public Mesh[]           baseMeshes     = new Mesh[0];
+    public Mesh[]           computedMeshes = new Mesh[0];
+#if UNITY_EDITOR
+
+    public static CoACDColliderData CreateAsset(string path, CoACD.Parameters parameters, Mesh[] meshes, Mesh[] baseMeshes)
+    {
+        var obj = CreateInstance<CoACDColliderData>();
+        AssetDatabase.CreateAsset(obj, path);
+        var c = 0;
+        foreach (var mesh in meshes) {
+            mesh.name = $"Computed Mesh {c++}";
+            AssetDatabase.AddObjectToAsset(mesh, obj);
+        }
+        obj.parameters     = parameters;
+        obj.baseMeshes     = baseMeshes;
+        obj.computedMeshes = meshes;
+        return obj;
+    }
+
+    public void UpdateAsset(CoACD.Parameters parameters, Mesh[] meshes, Mesh[] baseMeshes)
+    {
+        foreach (var mesh in computedMeshes) { AssetDatabase.RemoveObjectFromAsset(mesh); }
+        computedMeshes = null;
+        var c = 0;
+        foreach (var mesh in meshes) {
+            mesh.name = $"Computed Mesh {c++}";
+            AssetDatabase.AddObjectToAsset(mesh, this);
+        }
+        this.parameters = parameters;
+        this.baseMeshes = baseMeshes;
+        computedMeshes  = meshes;
+    }
+
+#endif
+}
