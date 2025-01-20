4c938a9d21f04a69aef6879b5cc9505fe5f01a0c
hatimbenhsain <hb1487@nyu.edu>
2025-01-20 12:00:36 -0500

    added roadbloacks in library and edge tunnel and edge1, imported misc sprites

---
+++ SwimmingGame/Assets/Scripts/Overworld/CurrentSphere.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CurrentSphere : MonoBehaviour
+{
+    public GameObject[] prefabs;
+    public float emitRadius=20f;
+    [Tooltip("Emit nothing in this direction.")]
+    public Vector3 emptyDirection;
+    [Tooltip("Max angle from empty direction where we're not emitting.")]
+    public float maxAngle=0f;
+    public float stepLength=5;
+    void Start()
+    {
+        for(float n=0f;n<180f;n+=stepLength){
+            float offset=Random.Range(-stepLength,stepLength);
+            for(float k=0f;k<360f;k+=stepLength){
+                Vector3 pos=new Vector3(Mathf.Cos((k+offset)*Mathf.PI/180f)*emitRadius,Mathf.Sin((k+offset)*Mathf.PI/180f)*emitRadius,0f);
+                Quaternion q=Quaternion.AngleAxis(n,Vector3.up);
+                pos=q*pos;
+                float angle=Vector3.Angle(pos,emptyDirection);
+                if(angle>maxAngle){
+                    GameObject g=Instantiate(prefabs[Random.Range(0,prefabs.Length-1)],transform.position+pos,Quaternion.LookRotation(-pos),transform);
+                }
+            }
+        }
+    }
+
+}
