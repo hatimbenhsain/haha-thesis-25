4822ce62c5f0219c885d8952736fa6492308ee76
hatimbenhsain <hb1487@nyu.edu>
2025-01-20 21:55:08 -0500

    added npc swimming animations + changing npc animation depending on where standing

--- SwimmingGame/Assets/Scripts/Migration/MigrationNPC.cs
+++ SwimmingGame/Assets/Scripts/Migration/MigrationNPC.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.U2D.Animation;
 
 public class MigrationNPC : MonoBehaviour
 {
     public float strokeFrequencyVariance=0.2f;
     public RuntimeAnimatorController [] animators;
+    public SpriteLibraryAsset[] spriteLibraryAssets;
 
     public float maxDistance;
 
@@ -19,7 +21,8 @@ public class MigrationNPC : MonoBehaviour
         spriteRenderer.material.color=Color.HSVToRGB(Random.Range(0f,1f),33f/255f,1f);
         NPCOverworld npcOverworld=GetComponent<NPCOverworld>();
         npcOverworld.strokeFrequency=npcOverworld.strokeFrequency+Random.Range(-strokeFrequencyVariance,strokeFrequencyVariance);
-        GetComponent<Animator>().runtimeAnimatorController=animators[Random.Range(0,animators.Length)];
+        //GetComponent<Animator>().runtimeAnimatorController=animators[Random.Range(0,animators.Length)];
+        GetComponentInChildren<SpriteLibrary>().spriteLibraryAsset=spriteLibraryAssets[Random.Range(0,spriteLibraryAssets.Length)];
         float s=Random.Range(0.9f,1.5f);
         spriteRenderer.transform.localScale=Vector3.one*s;
     }
