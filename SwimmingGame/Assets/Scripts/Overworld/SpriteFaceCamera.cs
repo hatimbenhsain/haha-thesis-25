cfdc1d68d78da69cebfc1b311f0ce1d228908ae9
hatimbenhsain <hb1487@nyu.edu>
2025-01-25 11:57:44 -0500

    added polybrush + plants at bottom

--- SwimmingGame/Assets/Scripts/Overworld/SpriteFaceCamera.cs
+++ SwimmingGame/Assets/Scripts/Overworld/SpriteFaceCamera.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class SpriteFaceCamera : MonoBehaviour
 {
+    public bool lockX=false;
+    public bool lockY=false;
+    public bool lockZ=false;
+
+    private Vector3 originalRotation;
     void Start()
     {
-        
+        originalRotation=transform.localRotation.eulerAngles;
     }
 
     void Update()
     {
         transform.rotation=Camera.main.gameObject.transform.rotation;
+        if(lockX || lockY || lockZ){
+            Vector3 rot=transform.localRotation.eulerAngles;
+            if(lockX) rot.x=originalRotation.x;
+            if(lockY) rot.y=originalRotation.y;
+            if(lockZ) rot.z=originalRotation.z;
+            transform.localRotation=Quaternion.Euler(rot);
+        }
     }
 }
