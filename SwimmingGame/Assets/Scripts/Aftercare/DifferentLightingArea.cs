5e58c4060142af0fdde639f7f94394059ccbbcca
hatimbenhsain <hb1487@nyu.edu>
2025-04-05 18:14:58 -0400

    added env light change to differentlightingarea script

--- SwimmingGame/Assets/Scripts/Aftercare/DifferentLightingArea.cs
+++ SwimmingGame/Assets/Scripts/Aftercare/DifferentLightingArea.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 
 public class DifferentLightingArea : MonoBehaviour
@@ -7,33 +9,56 @@ public class DifferentLightingArea : MonoBehaviour
     public Light directionalLight;
     private float directionalLightBaseIntensity;
     public float directionalLightInsideIntensity;
-    public float directionalLightLerpSpeed;
+    public float lerpSpeed=1f;
+
+    private float environmentalLightBaseIntensity;
+    public float environmentalLightInsideIntensity=-1f;
 
     private bool inside=false;
+
+    public bool active=false;
 
     void Start()
     {
         directionalLightBaseIntensity=directionalLight.intensity;
+        environmentalLightBaseIntensity=RenderSettings.ambientIntensity;
     }
 
     void Update()
     {
-        float directionalLightTargetIntensity;
-        if(inside) directionalLightTargetIntensity=directionalLightInsideIntensity;
-        else directionalLightTargetIntensity=directionalLightBaseIntensity;
-        directionalLight.intensity=Mathf.Lerp(directionalLight.intensity,directionalLightTargetIntensity,
-            directionalLightLerpSpeed*Time.deltaTime);
+        if(active){
+            float directionalLightTargetIntensity=directionalLightBaseIntensity;
+            float environmentalLightTargetIntensity=environmentalLightBaseIntensity;
+            if(inside){
+                if(directionalLightInsideIntensity!=-1f) directionalLightTargetIntensity=directionalLightInsideIntensity;
+                if(environmentalLightInsideIntensity!=-1f) environmentalLightTargetIntensity=environmentalLightInsideIntensity;
+            }
+            directionalLight.intensity=Mathf.Lerp(directionalLight.intensity,directionalLightTargetIntensity,
+                lerpSpeed*Time.deltaTime);
+            RenderSettings.ambientIntensity=Mathf.Lerp(RenderSettings.ambientIntensity,environmentalLightTargetIntensity,
+                lerpSpeed*Time.deltaTime);
+        }
     }
 
     void OnTriggerEnter(Collider other){
         if(other.gameObject.tag=="Player"){
             inside=true;
+            DeactivateOtherAreas();
         }
+        
     }
 
     void OnTriggerExit(Collider other){
         if(other.gameObject.tag=="Player"){
             inside=false;
+            DeactivateOtherAreas();
         }
+    }
+
+    void DeactivateOtherAreas(){
+        foreach(DifferentLightingArea differentLightingArea in FindObjectsOfType<DifferentLightingArea>()){
+            differentLightingArea.active=false;
+        }
+        active=true;
     }
 }
