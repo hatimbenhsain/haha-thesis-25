f621ed0f232f82d44d8923a6bc1abf09622b158d
HanwenXuMame <128509287+HanwenXuMame@users.noreply.github.com>
2025-04-18 19:22:10 -0400

    fixed intro singing

--- SwimmingGame/Assets/Scripts/Aftercare/DifferentLightingArea.cs
+++ SwimmingGame/Assets/Scripts/Aftercare/DifferentLightingArea.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
-using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
