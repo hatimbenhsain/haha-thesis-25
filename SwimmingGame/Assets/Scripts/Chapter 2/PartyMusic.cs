5131c282fb5e8cb03dffc0d64ca205020ca425c7
hatimbenhsain <hb1487@nyu.edu>
2025-04-20 16:34:16 -0400

    added speaker loudness + gave beads sprites

--- SwimmingGame/Assets/Scripts/Chapter 2/PartyMusic.cs
+++ SwimmingGame/Assets/Scripts/Chapter 2/PartyMusic.cs
@@ -8,10 +8,26 @@ public class PartyMusic : MonoBehaviour
 {
     private Dialogue dialogue;
     private EventInstance instance;
+
+    private Transform[] speakers;
+    private Transform player;
+    [Tooltip("Further than this proximity do not boost music volume")]
+    public float maxProximity=5f;
+    [Tooltip("Closer than this proximity music volume is at max")]
+    public float minProximity=1f;
+
     void Start()
     {
         instance=GetComponent<StudioEventEmitter>().EventInstance;
         dialogue=FindObjectOfType<Dialogue>();
+
+        GameObject[] gs=GameObject.FindGameObjectsWithTag("Speaker");
+        speakers=new Transform[gs.Length];
+        for(var i=0;i<gs.Length;i++){
+            speakers[i]=gs[i].transform;
+        }
+
+        player=GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     void Update()
@@ -21,6 +37,14 @@ public class PartyMusic : MonoBehaviour
         }else{
             instance.setParameterByName("Music Intensity",0f);
         }
+
+        float proximityToSpeakers=100f;
+        foreach(Transform t in speakers){
+            float d=Vector3.Distance(t.position,player.position);
+            proximityToSpeakers=Mathf.Min(d,proximityToSpeakers);
+        }
+        proximityToSpeakers=Mathf.Clamp(proximityToSpeakers,minProximity,maxProximity);
+        instance.setParameterByName("proximityToSpeaker",(maxProximity-proximityToSpeakers)/(maxProximity-minProximity));
     } 
 
     public bool IsMuffled(){
